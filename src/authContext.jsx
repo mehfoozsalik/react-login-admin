5b78e09fe29d59777f2a996b480f731ce8669e95
mehfoozsalik <mehfoozsalik@gmail.com>
2022-09-30 20:55:33 +0500

    new

---
+++ src/authContext.jsx
@@ -0,0 +1,67 @@
+import React, { useReducer } from "react";
+import MkdSDK from "./utils/MkdSDK";
+
+export const AuthContext = React.createContext();
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  token: null,
+  role: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      console.log(action.payload)
+      return {
+        ...state,
+        isAuthenticated:true,
+        user: action.payload.user_id,
+        token: action.payload.token,
+        role: action.payload.role
+      };
+    case "LOGOUT":
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
+
+let sdk = new MkdSDK();
+
+export const tokenExpireError = (dispatch, errorMessage) => {
+  const role = localStorage.getItem("role");
+  if (errorMessage === "TOKEN_EXPIRED") {
+    dispatch({
+      type: "Logout",
+    });
+    window.location.href = "/" + role + "/login";
+  }
+};
+
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  React.useEffect(() => {
+    //TODO
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
